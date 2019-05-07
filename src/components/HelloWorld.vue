5a8c0b98fea08f06eec87c9d971bcbedc4992bf9
ticak <tijanak1991@hotmail.com>
2019-05-07 21:14:31 +0200

    zzz

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,76 +1,61 @@
 <template>
   <div v-bind:id="randomId">
-    <p>{{name}} </p>
+    <p>{{name}}</p>
     <p>{{randomId + 100}}</p>
     <p>{{animals[0]}}</p>
-    <p> {{name.split('').reverse().join('')}} </p>
-    <p>{{randomId%2===0 ? "Deljiv":"Nije deljiv"}}</p>
+    <p>{{name.split('').reverse().join('')}}</p>
+    <p>{{randomId%2===0 ? "Deljiv" : "Nije deljiv"}}</p>
 
-    <div v-if="name==='Tijasna'">
-      hello {{name}}
-    </div>
+    <div v-if="name==='Tijasna'">hello {{name}}</div>
 
-    <div v-else>
-      Hello someone
-    </div>
+    <div v-else>Hello someone</div>
 
-    <button v-on:click="handleClick">Click me</button>
-    <a :href ="link">Link </a>
+    <!-- <button v-on:click="handleClick">Click me</button> -->
+    <button @click="handleClick">Click me</button>
+
+    <a :href="link">Google</a>
 
     <form @submit.prevent="handleSubmit">
       <button type="submit">Submit</button>
     </form>
 
-  <div :class="{'red' : isRed}">
-      I'm red
-  </div>
+    <div :class="{'red' : isRed}">I'm red</div>
 
-  <ul>
+    <ul>
+      <li v-for="(color,index) in colors" :key="color">{{index+1}} {{color}}</li>
+    </ul>
 
-    <li v-for="(color,index) in colors" :key="color">
-     {{index+1}} {{color}}
-    </li>
+    <input type="text" v-model="name">
 
-  </ul>
-
-  <input type="text" v-model="name" />
-
-  <button @click="myFun">Greet</button>
-
-
-
+    <button @click="myFun">Greet</button>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
-      name:"Tijana",
+      name: "Tijana",
       randomId: Math.random(),
-      animals:['dog','cat'],
-      link:"https://www.google.com/",
-      isRed:true,
-      colors:[
-        'green',
-        'red',
-        'blue'
-      ]
-    }
+      animals: ["dog", "cat"],
+      link: "https://www.google.com/",
+      isRed: true,
+      colors: ["green", "red", "blue"]
+    };
   },
-  methods:{
-    handleClick(){
-      console.log('CLICKED');
+  methods: {
+    handleClick() {
+      console.log("CLICKED");
     },
-    handleSubmit(){
-      console.log('Submitted');
+    handleSubmit() {
+      console.log("Submitted");
     },
-    myFun(){
+    myFun() {
       alert(`Hello ${this.name}`);
+      // alert("Hello" + this.name);
     }
   }
-
-}
+};
 </script>
 
 <style>
